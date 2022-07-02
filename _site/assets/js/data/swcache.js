e4e7f3965c9cfba1235b2f5c94dc0ab8cf8caeda
Dhafo <krigen.y@gmail.com>
2022-07-01 23:09:38 -0400

    t

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,61 @@
+const resource = [
+
+  /* --- CSS --- */
+  '/Dhafo.github.io/assets/css/style.css',
+
+  /* --- PWA --- */
+  '/Dhafo.github.io/app.js',
+  '/Dhafo.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/Dhafo.github.io/index.html',
+  '/Dhafo.github.io/404.html',
+  
+    '/Dhafo.github.io/categories/',
+  
+    '/Dhafo.github.io/tags/',
+  
+    '/Dhafo.github.io/archives/',
+  
+    '/Dhafo.github.io/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/Dhafo.github.io/assets/img/favicons/android-chrome-192x192.png',
+    '/Dhafo.github.io/assets/img/favicons/android-chrome-512x512.png',
+    '/Dhafo.github.io/assets/img/favicons/apple-touch-icon.png',
+    '/Dhafo.github.io/assets/img/favicons/favicon-16x16.png',
+    '/Dhafo.github.io/assets/img/favicons/favicon-32x32.png',
+    '/Dhafo.github.io/assets/img/favicons/favicon.ico',
+    '/Dhafo.github.io/assets/img/favicons/mstile-150x150.png',
+    '/Dhafo.github.io/assets/js/dist/categories.min.js',
+    '/Dhafo.github.io/assets/js/dist/commons.min.js',
+    '/Dhafo.github.io/assets/js/dist/home.min.js',
+    '/Dhafo.github.io/assets/js/dist/misc.min.js',
+    '/Dhafo.github.io/assets/js/dist/page.min.js',
+    '/Dhafo.github.io/assets/js/dist/post.min.js',
+    '/Dhafo.github.io/assets/js/dist/pvreport.min.js'
+
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'Dhafo.github.io',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
